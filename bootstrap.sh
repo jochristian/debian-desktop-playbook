3596202ea86cf7fd1ed9f0eb9bee4ef39f496754
jhx <jhx0x00@gmail.com>
2026-01-21 21:51:22 +0100

    first customization

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,6 @@ su -c "apt install ansible sshpass -y"
 
 echo "[INFO] Running Playbook"
 export ANSIBLE_HOST_KEY_CHECKING=False
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 ansible-playbook -i localhost, main.yml -Kk
